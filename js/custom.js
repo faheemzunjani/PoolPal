869a80c390ed92a8941f9995066de93369816651
Faheem Zunjani <faheemzunjani@gmail.com>
2018-10-20 13:52:46 +0530

    Modify custom.js

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
    var options = document.querySelectorAll('option');
    var instances = M.FormSelect.init(elems, options); 
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var options = document.querySelectorAll('option');
+    var instances = M.Modal.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = document.querySelectorAll('option');
+    var instances = M.Sidenav.init(elems, options);
+  });
